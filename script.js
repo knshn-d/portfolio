1c7158dff08993a3201c12ccb96c2b6933667a09
ken <2211360@slu.edu.ph>
2024-12-06 01:13:21 +0800

    add font improve design

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener('mousemove', function(e) {
+document.addEventListener('mousemove', function (e) {
     const background = document.querySelector('.background');
     const foreground = document.querySelector('.foreground');
-    
+
     const mouseX = e.clientX;
     const mouseY = e.clientY;
-    
+
     background.style.transform = `translate(${(mouseX - window.innerWidth / 2) / 30}px, ${(mouseY - window.innerHeight / 2) / 20}px)`;
     foreground.style.transform = `translate(${(mouseX - window.innerWidth / 2) / 20}px, ${(mouseY - window.innerHeight / 2) / 10}px)`;
 });
